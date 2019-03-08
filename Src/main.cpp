d8401399e4d2a0db777e63f4ca8f043b9664ee52
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2019-03-08 21:45:47 +0100

    Add files via upload

--- Src/main.cpp
+++ Src/main.cpp
@@ -35,6 +35,148 @@ void check_acknowledge(VCHAR const & rep_tram)
 
     throw Error(9,"Erreur inconnue du avec le serveur: ",Error::niveau::ERROR);
 }
+void _get_conf(gp2::Conf_param const& cp,VCHAR const & r_data,Tram & t_data,gp2::Data & gc)
+{
+
+    try
+    {
+        gp2::Get_config(cp,gc);
+
+        t_data+=(char)Tram::Com_bytes::ACK;
+
+        for(auto & i :gc)
+        {
+            t_data+=i;
+            t_data+=(char)Tram::Com_bytes::US;
+        }
+    }
+    catch(Error & e)
+    {
+        std::cerr<< e.what() <<std::endl;
+
+        t_data+=(char)Tram::Com_bytes::NAK;
+        t_data+=e.str();
+    }
+}
+
+void _set_conf(gp2::Conf_param const& cp,VCHAR const & r_data,Tram & t_data)
+{
+    try
+    {
+        std::string value("");
+
+        for(auto i=3;i<r_data.size();i++)
+        {
+            if(r_data[i]==Tram::Com_bytes::EOT)
+            {
+                gp2::Set_config(cp,value,false);
+                break;
+            }
+
+            value+=(char)r_data[i];
+        }
+
+        t_data+=(char)Tram::Com_bytes::ACK;
+    }
+    catch(Error & e)
+    {
+        std::cerr<< e.what() <<std::endl;
+
+        t_data+=(char)Tram::Com_bytes::NAK;
+        t_data+=e.str();
+    }
+}
+
+void treatement(VCHAR const & r_data,Tram & t_data)
+{
+    struct gp2::mnt _mnt{"gio mount",""};
+    t_data.clear();
+    t_data+=(char)Tram::Com_bytes::SOH;
+
+    if(r_data[1]==(char)RC_Apn::Com_bytes::Check_Apn)
+    {
+        gp2::Data device;
+        gp2::Auto_detect(device);
+
+        if(device.size()>0)
+        {
+            t_data+=(char)Tram::Com_bytes::ACK;
+            gp2::Unmount(_mnt);
+        }
+        else
+        {
+            t_data+=(char)Tram::Com_bytes::NAK;
+            t_data+="aucun apn detecté";
+        }
+    }
+    else if(r_data[1]==(char)RC_Apn::Com_bytes::Older)
+    {
+        t_data+=(char)Tram::Com_bytes::ACK;
+    }
+
+    else if(r_data[1]==(char)RC_Apn::Com_bytes::Debug_mode)
+    {
+        t_data+=(char)Tram::Com_bytes::ACK;
+    }
+    else if(r_data[1]==(char)RC_Apn::Com_bytes::Get_Config)//simplifiable par une fonction parsé
+    {
+        gp2::Data gc;
+
+        if(r_data[2]==RC_Apn::Com_bytes::Aperture)
+            _get_conf(gp2::Conf_param::APERTURE,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::Shutterspeed)
+            _get_conf(gp2::Conf_param::SHUTTERSPEED,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::Iso)
+            _get_conf(gp2::Conf_param::ISO,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::Format)
+            _get_conf(gp2::Conf_param::FORMAT,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::Target)
+            _get_conf(gp2::Conf_param::TARGET,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::White_balance)
+            _get_conf(gp2::Conf_param::WHITE_BALANCE,r_data,t_data,gc);
+        else if(r_data[2]==RC_Apn::Com_bytes::Picture_style)
+            _get_conf(gp2::Conf_param::PICTURE_STYLE,r_data,t_data,gc);
+        else
+        {
+            t_data+=(char)Tram::Com_bytes::NAK;
+            t_data+="demande de Get_Config inconnue: ";
+            t_data+=r_data[2];
+        }
+    }
+    else if(r_data[1]==(char)RC_Apn::Com_bytes::Set_Config)
+    {
+        if(r_data[2]==(char)RC_Apn::Com_bytes::Aperture)
+            _set_conf(gp2::Conf_param::APERTURE,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::Shutterspeed)
+            _set_conf(gp2::Conf_param::SHUTTERSPEED,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::Iso)
+            _set_conf(gp2::Conf_param::ISO,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::Format)
+            _set_conf(gp2::Conf_param::FORMAT,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::Target)
+            _set_conf(gp2::Conf_param::TARGET,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::White_balance)
+            _set_conf(gp2::Conf_param::WHITE_BALANCE,r_data,t_data);
+        else if(r_data[2]==RC_Apn::Com_bytes::Picture_style)
+            _set_conf(gp2::Conf_param::PICTURE_STYLE,r_data,t_data);
+        else
+        {
+            t_data+=(char)Tram::Com_bytes::NAK;
+            t_data+="demande de Set_Config inconnue: ";
+            t_data+=r_data[2];
+        }
+    }
+    else
+    {
+        t_data+=(char)Tram::Com_bytes::NAK;
+        t_data+="commande inconnue: ";
+        t_data+=r_data[1];
+    }
+
+
+    t_data+=(char)Tram::Com_bytes::EOT;
+
+}
 
 void init_configure(struct gp2::mnt & _mount)
 {
@@ -145,17 +287,22 @@ int main(int argc,char ** argv)
         try
         {
             #ifdef __DEBUG_MODE
-                std::clog << "Tram: "<< std::hex;
+                 std::clog << "Tram R: ("<< Request.size() << " octets) " <<std::hex;
                 for(auto & i:Request.get_data())
                     std::clog <<"0x"<< static_cast<int>(i) << " ";
-                std::clog  << std::dec << std::endl;
+                std::clog  << std::dec << std::endl<< std::endl;
             #endif // __DEBUG_MODE
 
             check_acknowledge(Request.get_c_data());
 
-            Respond+=(char)Tram::Com_bytes::SOH;
-            Respond+=(char)Tram::Com_bytes::ACK;
-            Respond+=(char)Tram::Com_bytes::EOT;
+            treatement(Request.get_c_data(),Respond);
+
+            #ifdef __DEBUG_MODE
+                std::clog << "Tram T: ("<< Respond.size() << " octets) " <<std::hex;
+                for(auto & i:Respond.get_data())
+                    std::clog <<"0x"<< static_cast<int>(i) << " ";
+                std::clog  << std::dec << std::endl<< std::endl;
+            #endif // __DEBUG_MODE
 
             Server.Write(Id_Socket,Respond.get_c_data());
         }
